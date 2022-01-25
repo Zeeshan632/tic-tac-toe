d3ce579e195ef1a4eed5693a097fa0fdbcd13fd6
Zeeshan632 <zeeshanadil632@gmail.com>
2022-01-25 15:31:16 +0500

    made it responsive but still has a bug about when the game ties

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const allBlocks = document.querySelectorAll(".input-block");
 const turnsText = document.querySelector(".turns");
 
 let recentMove = 0;
+let blocksClicked = 0; // later on we will check if all the blocks are clicked or not, to decide if somewon wins or the game ties
 
 for (let block of allBlocks) {
 	block.addEventListener("click", (e) => {
@@ -10,10 +11,14 @@ for (let block of allBlocks) {
 			e.target.classList.add("cross");
 			e.target.innerHTML = "<h1 class='cross-tag'>X</h1>";
 			recentMove = 1;
+
+			blocksClicked++;
 		} else if (recentMove !== 0 && !e.target.classList.contains("cross")) {
 			e.target.classList.add("zero");
 			e.target.innerHTML = "<h1 class='zero-tag'>O</h1>";
 			recentMove = 0;
+
+			blocksClicked++;
 		}
 
 		// checking for turns and updating the text
@@ -35,6 +40,7 @@ let winningConditions = [
 	[2, 4, 6],
 ];
 container.addEventListener("click", (e) => {
+	// the following two variable are the arrays, which contain the indexes of the blocks whether they have a cross class or zero class
 	let ifCrossWin = [];
 	let ifZeroWin = [];
 	if (e.target.classList.contains("input-block")) {
@@ -46,12 +52,13 @@ container.addEventListener("click", (e) => {
 			}
 		}
 	}
-	// console.log(ifCrossWin, ifZeroWin);
 
+	// the following is checking if after any click, whether the indexes of the 'winninCondtions' meets the indexes stored in either 'ifZeroWin' or 'ifCrossWin', if one single digits matches, then either the crossWinCount or zeroWinCount gets one added to it
+	let crossWinCount;
+	let zeroWinCount;
 	for (let condition of winningConditions) {
-		let crossWinCount = 0;
-		let zeroWinCount = 0;
-
+		crossWinCount = 0;
+		zeroWinCount = 0;
 		for (let i = 0; i < condition.length; i++) {
 			if (ifCrossWin.includes(condition[i])) {
 				crossWinCount++;
@@ -71,5 +78,12 @@ container.addEventListener("click", (e) => {
 				location.reload();
 			}, 500);
 		}
+	}
+
+	// checking if all the blocks are clicked
+	if (blocksClicked === 9) {
+		setTimeout(() => {
+			alert("it's a tie");
+		}, 1000);
 	}
 });
